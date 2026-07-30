39a76e3fda96b26ad9c84ebab03ebc329b9de4b7
Rinor Rexhaj <105825061+RinorRexhaj@users.noreply.github.com>
2026-07-30 16:11:02 +0200

    Commented out the villa vibes and management system projectss

--- src/utils/Projects.ts
+++ src/utils/Projects.ts
@@ -186,36 +186,36 @@ export const projects: Project[] = [
       },
     ],
   },
-  {
-    title: "Villa Vibes",
-    description: "Luxury villa booking platform with virtual tours.",
-    images: [
-      {
-        src: "/assets/img/villa-home.jpg",
-        alt: "VillaVibes Home Page",
-        width: 1920,
-        height: 1080,
-      },
-      {
-        src: "/assets/img/villa-about.jpg",
-        alt: "VillaVibes About Page",
-        width: 1920,
-        height: 1080,
-      },
-      {
-        src: "/assets/img/villa-chat.jpg",
-        alt: "VillaVibes Chat Feature",
-        width: 1920,
-        height: 1080,
-      },
-      {
-        src: "/assets/img/villa-book.jpg",
-        alt: "VillaVibes Booking Page",
-        width: 1920,
-        height: 1080,
-      },
-    ],
-  },
+  // {
+  //   title: "Villa Vibes",
+  //   description: "Luxury villa booking platform with virtual tours.",
+  //   images: [
+  //     {
+  //       src: "/assets/img/villa-home.jpg",
+  //       alt: "VillaVibes Home Page",
+  //       width: 1920,
+  //       height: 1080,
+  //     },
+  //     {
+  //       src: "/assets/img/villa-about.jpg",
+  //       alt: "VillaVibes About Page",
+  //       width: 1920,
+  //       height: 1080,
+  //     },
+  //     {
+  //       src: "/assets/img/villa-chat.jpg",
+  //       alt: "VillaVibes Chat Feature",
+  //       width: 1920,
+  //       height: 1080,
+  //     },
+  //     {
+  //       src: "/assets/img/villa-book.jpg",
+  //       alt: "VillaVibes Booking Page",
+  //       width: 1920,
+  //       height: 1080,
+  //     },
+  //   ],
+  // },
   {
     title: "PowerDash",
     description:
@@ -265,60 +265,60 @@ export const projects: Project[] = [
       },
     ],
   },
-  {
-    title: "Management System",
-    description: "Business management solution with robust features.",
-    images: [
-      {
-        src: "/assets/img/sign-in.jpg",
-        alt: "Sign In Page",
-        width: 1920,
-        height: 1080,
-      },
-      {
-        src: "/assets/img/dashboard-1.jpg",
-        alt: "Main Dashboard",
-        width: 1920,
-        height: 1080,
-      },
-      {
-        src: "/assets/img/dashboard-2.jpg",
-        alt: "Secondary Dashboard",
-        width: 1920,
-        height: 1080,
-      },
-      {
-        src: "/assets/img/notifications.jpg",
-        alt: "Notifications Panel",
-        width: 1920,
-        height: 1080,
-      },
-      {
-        src: "/assets/img/top-cars.jpg",
-        alt: "Top Cars View",
-        width: 1920,
-        height: 1080,
-      },
-      {
-        src: "/assets/img/orders.jpg",
-        alt: "Orders Management",
-        width: 1920,
-        height: 1080,
-      },
-      {
-        src: "/assets/img/reservations.jpg",
-        alt: "Reservations System",
-        width: 1920,
-        height: 1080,
-      },
-      {
-        src: "/assets/img/settings.jpg",
-        alt: "Settings Panel",
-        width: 1920,
-        height: 1080,
-      },
-    ],
-  },
+  // {
+  //   title: "Management System",
+  //   description: "Business management solution with robust features.",
+  //   images: [
+  //     {
+  //       src: "/assets/img/sign-in.jpg",
+  //       alt: "Sign In Page",
+  //       width: 1920,
+  //       height: 1080,
+  //     },
+  //     {
+  //       src: "/assets/img/dashboard-1.jpg",
+  //       alt: "Main Dashboard",
+  //       width: 1920,
+  //       height: 1080,
+  //     },
+  //     {
+  //       src: "/assets/img/dashboard-2.jpg",
+  //       alt: "Secondary Dashboard",
+  //       width: 1920,
+  //       height: 1080,
+  //     },
+  //     {
+  //       src: "/assets/img/notifications.jpg",
+  //       alt: "Notifications Panel",
+  //       width: 1920,
+  //       height: 1080,
+  //     },
+  //     {
+  //       src: "/assets/img/top-cars.jpg",
+  //       alt: "Top Cars View",
+  //       width: 1920,
+  //       height: 1080,
+  //     },
+  //     {
+  //       src: "/assets/img/orders.jpg",
+  //       alt: "Orders Management",
+  //       width: 1920,
+  //       height: 1080,
+  //     },
+  //     {
+  //       src: "/assets/img/reservations.jpg",
+  //       alt: "Reservations System",
+  //       width: 1920,
+  //       height: 1080,
+  //     },
+  //     {
+  //       src: "/assets/img/settings.jpg",
+  //       alt: "Settings Panel",
+  //       width: 1920,
+  //       height: 1080,
+  //     },
+  //   ],
+  // },
   {
     title: "DragonBall Destiny",
     description: "Fast-paced fighting game, battling in Dragon Ball fashion.",
